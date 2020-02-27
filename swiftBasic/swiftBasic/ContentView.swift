44c127f58afe82f0e2e30bb2fe443d606587d28c
sungjin <jazzhong1@naver.com>
2020-02-27 22:55:26 +0900

    swiftBasic 프로젝트 추가

---
+++ swiftBasic/swiftBasic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftBasic
+//
+//  Created by SungJin Hong on 2020/02/27.
+//  Copyright © 2020 홍성진. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
