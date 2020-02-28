48a9e3499848ecc02f639fab492f7708db73b1c2
sungjin <jazzhong1@naver.com>
2020-02-28 21:33:06 +0900

    swift grammar추가

---
+++ grammar/BasicOperation.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+//Assignment Operators 할당 연산자
+let b = 10; //선언 시 처음 입력된 데이터만 저장.
+var a = 5; //선언시 처음 입력된 데이터 이후 추가 입력가능
+a = b;
+print(a);
+
+let (x,y) = (1,2);
+print(x,y);
+
+//Arithmetic Operators 사칙 연산자
+1 + 2;
+5 - 3;
+2 * 3;
+10.0 / 2.5;
+
+"hello, " + "world";
+
+//Remainder Operator
+9 % 3
+-9 % 4
+
+//Unary Minus Operator
+//숫자 값은 - 로 표현되는 단항 음수 연산자에 의해 부호가 변함
+let three = 3;
+let minusThree = -three;
+let plusThree = -minusThree;
+
+//Unary Plus Operator
+//+로 표현되는 단항 양수 연산자는 부호에 아무련 영향 X
+let minusSix = -6;
+let alsoMinusSix = +minusThree;
+
+
+
+
+
