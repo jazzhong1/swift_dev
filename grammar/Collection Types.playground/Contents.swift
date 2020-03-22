6d409ad5ccf5511f3fba6da363cd22eb9fad31db
sungjin <jazzhong1@naver.com>
2020-03-22 16:55:22 +0900

    Collection완료

--- grammar/Collection Types.playground/Contents.swift
+++ grammar/Collection Types.playground/Contents.swift
@@ -39,6 +39,121 @@ print("The shopping list contains \(shoppingList.count) items.")
 shoppingList.append("Four");
 shoppingList.count;
 
+shoppingList += ["Baking Powder"];
+shoppingList += ["Chocolate Spread", "Cheese", "Butter"];
+
+//배열 특정 위치의 원소 접근
+var firstItem  = shoppingList[0];
+
+shoppingList[4...6] = ["Bananas", "Apples"];
+print(shoppingList);
+
+//특정 위치에 원소 추가/삭제/접근
+shoppingList.insert("Maple Syrp", at: 0);
+let mapleSyrup = shoppingList.remove(at: 0);
+print(mapleSyrup);
+firstItem = shoppingList[0];
+//firstItem = "Six eggs";
+
+let apples = shoppingList.removeLast();
+
+for item in shoppingList {
+    print(item);
+}
+
+//배열 인덱스가 필요할때 enumerated()메소드.
+for (index,value) in shoppingList.enumerated() {
+    print("Item \(index + 1) : \(value)");
+}
+
+
+
+//Set
+//set 형태로 저장되기 위해서는 반드시 타입이 hashable  이여야 한다
+//Swift에서 String, Int, Double, Bool 같은 기본타입은 기본적으로 hashable이다.
+//Set 타입은 Set으로 선언한다...
+var letter = Set<Character>();
+print("letter is of type Set<Character> with \(letter.count) item");
+letter.insert("a");
+letter = [];
+
+//배열 리터럴을 이용한 Set생성
+var favoriteGenres : Set<String> = ["Rock", "Classical", "Hip hop"];
+
+//Swift의 타입추론으로 아래와 같이 선언 가능..
+var _favoriteGenres: Set = ["AAA","BBB","CCC"];
+print("I have \(favoriteGenres.count)")
+
+if favoriteGenres.isEmpty{
+    print("As far as music goes, I'm not picky.");
+}else{
+    print("I have particual music preferences");
+}
+favoriteGenres.insert("Jazz");
+print("favoriteGenres \(favoriteGenres)")
+
+if let removedGenre = favoriteGenres.remove("Rock"){
+    print("\(removedGenre)? I'm over it");
+}else{
+    print("I never much cared for that");
+}
+
+if favoriteGenres.contains("Funk"){
+    print("I get up on the good foot");
+}else{
+    print("I never much cared that");
+}
+
+//Set순회
+//for-in loop를 이용해 set을 순회 할 수 있다.
+for genre in favoriteGenres{
+    print("\(genre)");
+}
+
+//Set명령
+let oddDigits: Set = [1,3,5,7,9];
+let evenDigits: Set = [0,2,4,6,8];
+let singleDigitPrimeNumber: Set = [2,3,5,7];
+
+oddDigits.union(evenDigits).sorted(); //합집합...
+oddDigits.intersection(evenDigits).sorted() //교집합..
+oddDigits.symmetricDifference(evenDigits).sorted(); //겹치지 않는애들
+oddDigits.subtracting(singleDigitPrimeNumber).sorted(); //oddDigits만 해당..
+
+
+let houseAnimals: Set = ["🐶", "🐱"];
+let farmAnimals: Set = ["🐮", "🐔", "🐑", "🐶", "🐱"];
+let cityAnimals: Set = ["🐦", "🐭"];
+houseAnimals.isSubset(of: farmAnimals);
+farmAnimals.isSuperset(of: houseAnimals);
+farmAnimals.isDisjoint(with: cityAnimals);//안겹치냐?
+
+var nameOfIntegers = [Int:String](); // dictionary생성
+nameOfIntegers[16]="sixteen";
+nameOfIntegers = [:];
+
+//리터럴을 이용한 Dictionary의 생성
+var airport : [String:String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"];
+print("The airports dictionary contains \(airport.count) items");
+
+//빈 Dictionary확인
+if airport.isEmpty{
+    print("is empty");
+}else{
+    print("is not empty");
+}
+
+airport["LIB"] = "London";
+
+
+
+
+
+
+
+
+
+
 
 
 
