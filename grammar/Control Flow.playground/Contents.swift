39ba7a96c7ad7df5cd8a75db5c4f7c49478bc305
sungjin <jazzhong1@naver.com>
2020-04-03 19:08:11 +0900

    repeat while 까지 완료

--- grammar/Control Flow.playground/Contents.swift
+++ grammar/Control Flow.playground/Contents.swift
@@ -31,6 +31,42 @@ print("\(base) to ther power is \(answer)");
 //범위 연산자와 함께 사용 할 수 있다.
 let minutes = 60;
 for tickMark in 0..<minutes{
-    print("tickMark \(tickMark+1)");
+    //print("tickMark \(tickMark+1)");
 }
+
+//strid(frm:tp:by:) 함수와 함께 상용가능... 구간 5로 설정한 경우
+//구간 5를 띄어가며 실행한다.
+//let minuteInterval = 5;
+//to: 뒤의 경계를 포함 하지 않는다.
+for tickMark in stride(from: 0, to: 12, by: 1){
+    print("check tickMar \(tickMark)");
+    //render the tick mark every 5 minute (0, 5, 10 ,15 .. 55)
+}
+
+//stride 구간 3으로 설정 한 경우
+//throung ClosedRange 처럼 뒤쪽 경계값을 포함.
+let hours = 12;
+let hourInterval = 3;
+for tickMark in stride(from: 0, through: hours, by: 1){
+    print("tickmark \(tickMark)");
+}
+
+//While문
+//Swift에서 While과 repeat-While 두가지 종루... do-while인듯...
+//condition이  false일 때까지 구문(statements)를 반복 한다.
+var square = 0;
+var dicRoll = 0;
+let finalSquare = 20;
+while square < finalSquare {
+    print("check square \(square)");
+    square+=1;
+}
+
+//repeat do while..로 쓰기도함........?
+square = 0;
+repeat{
+    print("repeat while \(square)");
+    square += 2;
+}while square < finalSquare
+
 
